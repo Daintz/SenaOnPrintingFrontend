8b47d97aafb2380ddebab88bb9f813d754fc2c44
Daniel Lopez Rubio <81586667+Daintz@users.noreply.github.com>
2023-06-05 14:57:35 -0500

    build: :lipstick: update more UI
    update more UI in dashboard

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -9,17 +9,16 @@ const Product = () => {
   const { isEditing } = useSelector((state) => state.modal)
 
   return (
-    <>
-      <div className="p-4 border-gray-200 border-dashed">
-        <div className="overflow-x-auto">
-          <ListProduct />
-          {/* Esta logica del modal esta acá para poder ser reutilizable */}
-          <NewModal>
-            {isEditing ? <UpdateProduct /> : <CreateProduct />}
-          </NewModal>
-        </div>
+    <div className="p-4 border-gray-200 border-dashed">
+      <div className="overflow-x-auto">
+        <ListProduct />
+        {/* Esta logica del modal esta acá para poder ser reutilizable */}
+        <NewModal>
+          {isEditing ? <UpdateProduct /> : <CreateProduct />}
+        </NewModal>
       </div>
-    </>
+    </div>
+
   )
 }
 
