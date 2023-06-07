d410f9771540acc547338c0664f5377bd6367ba5
Daniel Lopez Rubio <81586667+Daintz@users.noreply.github.com>
2023-06-07 14:30:37 -0500

    build: :iphone: improve responsive
    improve responsive design

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -7,7 +7,7 @@ const DashboardLayout = ({ children }) => {
       <Navbar />
       <Sidebar />
       <div className="mt-16 sidebarMD:ml-60">
-        <div className="py-10 sidebarMD:pl-[10%] sidebarMD:mr-[6%] 2xl:mr-[8%]">
+        <div className="py-10 sidebarMD:pl-[10%] sidebarMD:pr-[6%] 2xl:pr-[8%]">
           {children}
         </div>
       </div>
