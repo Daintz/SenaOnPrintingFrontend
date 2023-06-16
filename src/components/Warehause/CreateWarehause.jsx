165646a00e38bda281df7e1e634996abb0c01b2d
SantiagoPH32 <Santiago123ph@gmail.com>
2023-06-16 09:38:37 -0500

    chore: :hammer: update select in create warehause(falta update)

--- src/components/Warehause/CreateWarehause.jsx
+++ src/components/Warehause/CreateWarehause.jsx
@@ -1,4 +1,5 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import * as Yup from 'yup'
 import { usePostWarehauseMutation } from '../../context/Api/Common'
@@ -11,14 +12,48 @@ import {
 } from '../../context/Slices/Modal/ModalSlice'
 import Spinner from '../Spinner/Spinner'
 import { toast } from 'react-toastify'
+import clientAxios from '../../config/clientAxios'
 
-const validationSchema = Yup.object().shape({
+/* const validationSchema = Yup.object().shape({
   warehouseTypeId: Yup.string().required('Campo requerido'),
   name: Yup.string().required('Campo requerido'),
   ubication: Yup.string().required('Campo requerido')
+}) */
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Campo requerido'),
+  ubication: Yup.string().required('Campo requerido'),
+  warehouseTypeId: Yup.number().required('Campo requerido').moreThan(0, 'Debe elegir un tipo de boega'),
 })
 
+const getWarehausesType = () => {
+  return new Promise((resolve, reject) => {
+    clientAxios.get('/WarehauseType').then(
+      (result) => {
+        const warehausestype = result.data.map((warehausetype) => ({
+          'label': warehausetype.name,
+          'value': warehausetype.id
+        }));
+        resolve(warehausestype);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
+
 function CreateWarehause () {
+  const [warehauseTypeOptions, setWarehauseTypeOptions] = useState([]);
+  const fetchOptions = () => {
+    getWarehausesType().then((options) => {
+      setWarehauseTypeOptions(options);
+    });
+  };
+
+  useEffect(() => {
+    fetchOptions();
+  }, []);
+
   const dispatch = useDispatch()
   const [createWarehause, { error, isLoading }] = usePostWarehauseMutation()
 
@@ -45,10 +80,12 @@ function CreateWarehause () {
     {
       key: 1,
       name: 'warehouseTypeId',
-      title: 'Tipo',
-      type: 'text',
+      title: 'Tipo de bodega',
+      type: 'select',
+      data: warehauseTypeOptions,
       placeholder: 'Tipo de de bodega'
     },
+    
     {
       key: 2,
       name: 'ubication',
@@ -70,8 +107,25 @@ function CreateWarehause () {
       }}
       validationSchema={validationSchema}
     >
-        <Form className="space-y-6">
-          {inputs.map(input => (
+          <Form className="space-y-6">
+        {inputs.map(input => {
+          return input.type == "select" ?
+            <div key={input.key}>
+              <label htmlFor={input.name}>{input.title}</label>
+              <br />
+              <Field name={input.name} as={input.type} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5">
+                <option value="0">Seleccione {input.title}</option>
+                {input.data.map(option => (
+                  <option value={option.value}>{option.label}</option>
+                ))}
+              </Field>
+              <ErrorMessage
+                name={input.name}
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            :
             <div key={input.key}>
               <label htmlFor={input.name}>{input.title}</label>
               <Field
@@ -87,14 +141,14 @@ function CreateWarehause () {
                 className="text-red-500"
               />
             </div>
-          ))}
-          <button
-            type="submit"
-            className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-          >
-            Registrar Bodega
-          </button>
-        </Form>
+        })}
+        <button
+          type="submit"
+          className="w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Crear Usuario
+        </button>
+      </Form>
     </Formik>
   )
 }
