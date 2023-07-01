47ee3cb72728546ecccf0532e4317ee6a0e5433a
Daniel Lopez Rubio <81586667+Daintz@users.noreply.github.com>
2023-07-01 18:06:30 -0500

    feat: add reports
    Add reports in dashboard

--- src/components/SupplyCategory/ListSupplyCategory.jsx
+++ src/components/SupplyCategory/ListSupplyCategory.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { useTable, usePagination, useGlobalFilter } from 'react-table'
 import { useGetAllSupplyCategoryQuery } from '../../context/Api/Common'
@@ -6,17 +6,23 @@ import { UpdateButtonSupplyCategory } from './UpdateSupplyCategory'
 import { ChangeStateButtonSupplyCategory } from './ChangeStateSupplyCategory'
 import { CreateButtonSupplyCategory } from './CreateSupplyCategory'
 import { DetailsButtonSupplyCategory } from './DetailsSupplyCategory'
+import { BsFillFileEarmarkBreakFill } from 'react-icons/bs'
+import { useReactToPrint } from 'react-to-print'
+import ReportSupplyCategory from './ReportSupplyCategoy'
 
 const ListSupplyCategory = () => {
+  const tablePDF = useRef()
+
+  const generatePDF = useReactToPrint({
+    content: () => tablePDF.current,
+    documentTitle: 'Informe de categoria de insumos'
+  })
+
   // ? Esta linea de codigo se usa para llamar los datos, errores, y el estado de esta cargando las peticiones que se hacen api que se declararon en el context en Api/Common
   const { data: dataApi, refetch } = useGetAllSupplyCategoryQuery()
 
   // ? Este bloque de codigo hace que la pagina haga un refech al api para poder obtener los cambios hechos
   const { isAction } = useSelector((state) => state.modal)
-  useEffect(() => {
-    refetch()
-  }, [isAction])
-  // ?
 
   const columns = useMemo(() => [
     { Header: 'Nombre', accessor: 'name' },
@@ -35,6 +41,11 @@ const ListSupplyCategory = () => {
   ], [])
 
   const data = useMemo(() => (dataApi || []), [dataApi])
+
+  useEffect(() => {
+    refetch()
+  }, [isAction])
+  // ?
 
   const {
     getTableProps,
@@ -62,6 +73,22 @@ const ListSupplyCategory = () => {
   }
 
   return (
+    <>
+    <div className='hidden'>
+      <div ref={tablePDF}>
+        <ReportSupplyCategory dataApi={dataApi}/>
+      </div>
+    </div>
+    <div className="relative bg-white py-6 px-20 shadow-2xl mdm:py-6 mdm:px-8 mb-2">
+    <button
+      className="flex items-center justify-center border border-gray-400 text-black bg-green-600 hover:bg-white focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 gap-3"
+      onClick={ generatePDF }
+      type="button"
+    >
+      <BsFillFileEarmarkBreakFill />
+      Crear un informe
+    </button>
+    </div>
     <div className="relative bg-white py-10 px-20 shadow-2xl mdm:py-10 mdm:px-8">
       <div className="bg-white sm:rounded-lg overflow-hidden">
       <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 pb-6">
@@ -209,6 +236,7 @@ const ListSupplyCategory = () => {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
